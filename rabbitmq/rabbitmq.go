216a415a28d15a86afd60df617861465bd7b3f03
zikster3262 <zikster3262@gmail.com>
2022-11-28 12:13:02 +0100

    add chapters

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -23,6 +23,21 @@ func ConnectToRabbit() (*amqp.Channel, error) {
 	ch, err := conn.Channel()
 	utils.FailOnError("rabbitmq", err)
 
+	confirms := make(chan amqp.Confirmation)
+	ch.NotifyPublish(confirms)
+	go func() {
+		for confirm := range confirms {
+			if confirm.Ack {
+				utils.LogWithInfo("rabbitmq", "Confirmed")
+			} else {
+				utils.LogWithInfo("rabbitmq", "Failed")
+			}
+		}
+	}()
+
+	err = ch.Confirm(false)
+	utils.FailOnError("rabbitmq", err)
+
 	utils.LogWithInfo("rabbitmq", "connected to rabbitMQ")
 	return ch, err
 }
