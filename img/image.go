13e3c036c63fb66f59f579e0fdec9b723a2bb7b0
zikster3262 <zikster3262@gmail.com>
2022-11-30 23:18:06 +0100

    add scrapping

--- img/image.go
+++ img/image.go
@@ -1,8 +1,35 @@
 package img
 
+import (
+	"io"
+	"net/http"
+
+	"github.com/zikster3262/shared-lib/utils"
+)
+
 type Image struct {
 	Title    string `json:"title"`
 	Url      string `json:"url"`
 	Chapter  string `json:"chapter"`
 	Filename string `json:"filename"`
+}
+
+func (i Image) DownloadFile() []byte {
+
+	var client http.Client
+	resp, err := client.Get(i.Url)
+	if err != nil {
+		utils.FailOnCmpError("shared", "img", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			utils.FailOnCmpError("shared", "img", err)
+		}
+
+		return bodyBytes
+	}
+	return nil
 }
